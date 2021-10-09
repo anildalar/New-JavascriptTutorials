41866bcb4a78b51e9159d31c745d7dfe0626c4ff
anildalar <dollorinfotech@gmail.com>
2021-10-09 10:11:52 +0530

    new

---
+++ 6oct2021 Instructions.js
@@ -0,0 +1,44 @@
+Types of Function
+1. Predefined Function
+2. User Defined
+
+DRY Dont Repeat Yourself
+
+Variable can store 1 value at a time
+
+Array can store more than 1 value at a time
+
+Array are denoted by Square Bracket []
+
+Syntax
+var arrayName = [element1,element2,element3,.........];
+let arrayName = [member1,member2,member3,.........];
+const arrayName = [index0,index1,index2`,.........];
+
+arrayName[indexnumber]
+
+
+For Loop
+
+
+for(initialization;condition;increment/decrement){
+
+}
+
+JS Operators
+
++
+-
+*
+/
+=
+
++
+
+<=
+
+Increment Operator
+++ 
+
+Decrement Operator
+--
